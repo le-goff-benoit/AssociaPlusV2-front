e1b0443ecd648a7afb3a61d36dba078eb18aa132
Benoît Le Goff <benoit.le.goff@icloud.com>
2021-07-13 07:34:46 +0200

    Start of register page creation

---
+++ src/pages/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return <div>My Register Page</div>;
+};
+
+export default Register;
